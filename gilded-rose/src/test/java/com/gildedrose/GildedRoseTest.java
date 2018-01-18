061f16c1fd81f5a41fa1517f56c807179931ed4e
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-18 11:57:10 +0100

    green: created the first golden master test

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,14 @@
 package com.gildedrose;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class GildedRoseTest {
 
@@ -14,4 +20,28 @@ public class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    public void
+    update_the_product_quality_of_given_days() {
+        String[] args = {"4"};
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+
+        TexttestFixture.main(args);
+
+        System.out.flush();
+        assertThat(byteArrayOutputStream.toString(), is(loadGameResultFromFile(4)));
+    }
+
+    private String loadGameResultFromFile(int days) {
+        String gameResultFile = String.format("result-days-%d.txt", days);
+        System.out.print(gameResultFile);
+        try {
+            return IOUtils.toString(this.getClass().getResourceAsStream(gameResultFile), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+    }
 }
